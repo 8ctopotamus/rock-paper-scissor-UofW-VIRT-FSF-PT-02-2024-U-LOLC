b871ed4ffaaf96ba2e7d543d5c33dfbc3d05e74a
Josh <zylo.codes@gmail.com>
2024-03-14 21:27:47 -0500

    repeats game with while loop

--- game.js
+++ game.js
@@ -1,39 +1,39 @@
 let humanScore = 0
 let computerScore = 0
 
+let isRunning = true
+
 const choices = ['r', 'p', 's']
 
-// TODO: while playing the game (bonus) 
+while (isRunning) {
+  const humanChoice = prompt("Choose r, p, or s").toLowerCase()
 
-const humanChoice = prompt("Choose r, p, or s").toLowerCase()
-
-// check for invalid input
-if (!choices.includes(humanChoice)) {
-  alert('Invalid input. You need to choose r, p, or s.')
+  // check for invalid input
+  if (!choices.includes(humanChoice)) {
+    alert('Invalid input. You need to choose r, p, or s.')
+  }
+  
+  const randomIndex = Math.floor( Math.random() * choices.length )
+  const computerChoice = choices[randomIndex]
+  
+  if (
+    (humanChoice === 'r' && computerChoice === 's') || 
+    (humanChoice === 'p' && computerChoice === 'r') || 
+    (humanChoice === 's' && computerChoice === 'p')
+  ) { 
+    alert("you won!")
+    humanScore++
+  } else if (humanChoice === computerChoice) { // if tie
+    alert("it was a tie!")
+  } else { 
+    // computer won
+    alert("you lost!")
+    computerScore++
+  }
+  
+  alert(`Human: ${humanScore} | Computer: ${computerScore}`)
+  
+  isRunning = confirm('do you want to play again?')
 }
 
-const randomIndex = Math.floor( Math.random() * choices.length )
-const computerChoice = choices[randomIndex]
-
-if (
-  (humanChoice === 'r' && computerChoice === 's') || 
-  (humanChoice === 'p' && computerChoice === 'r') || 
-  (humanChoice === 's' && computerChoice === 'p')
-) { 
-  alert("you won!")
-  humanScore++
-} else if (humanChoice === computerChoice) { // if tie
-  alert("it was a tie!")
-} else { 
-  // computer won
-  alert("you lost!")
-  computerScore++
-}
-
-alert(`Human: ${humanScore} | Computer: ${computerScore}`)
-
-// do you want to play again?
-  // if yes
-    // repeat the game
-  // if no
-    // stop
+alert('Goodbye')
